ad9c9ad905dde472e0afbedb0b2bc5183e859ea6
sdgandhi <sdgandhi@users.noreply.github.com>
2020-08-01 13:30:00 -0700

    Add image anchor to Reality project, finish light changing playground

--- Hue.playground/Contents.swift
+++ Hue.playground/Contents.swift
@@ -6,9 +6,9 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 class SelfSignedCertificateDelegate: NSObject, URLSessionDelegate {
     public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-       // Trust the certificate even if not valid
-       let urlCredential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
-       completionHandler(.useCredential, urlCredential)
+        // Trust the certificate even if not valid
+        let urlCredential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+        completionHandler(.useCredential, urlCredential)
     }
 }
 
@@ -26,12 +26,12 @@ var getLights: URLRequest = {
 }()
 
 struct LightState: Codable {
-    var on: Bool
-    var bri: Int
-    var hue: Int
-    var sat: Int
-    var effect: String
-    var reachable: Bool
+    var on: Bool = true
+    var bri: Int?
+    var hue: Int?
+    var sat: Int?
+    var effect: String?
+    var reachable: Bool?
     var transitiontime: Int?
 }
 
@@ -42,23 +42,42 @@ struct Light: Codable {
     var state: LightState
 }
 
-let cancellable = session.dataTaskPublisher(for: getLights)
-    .sink { completion in
-        switch completion {
-        case .failure(let error):
-            print(error)
-        case .finished:
-            print("Success")
-        }
-    } receiveValue: { value in
+//typealias DataTaskValue = (data: Data, response: URLResponse)
+//typealias DataTaskPublisher = AnyPublisher<DataTaskValue, URLError>
+
+let c = session.dataTaskPublisher(for: getLights)
+    .flatMap { (value) -> Publishers.Zip3<URLSession.DataTaskPublisher, URLSession.DataTaskPublisher, URLSession.DataTaskPublisher> in
         let lightsDictionary = (try? JSONDecoder().decode([String: Light].self, from: value.data))
         let lights = lightsDictionary?.map({ (key, value) -> Light in
             var light = value
             light.id = key
             return light
         })
-        lights?.forEach({ light in
-            print(light)
+        let lightChangePublishers = lights!.map({ (light) -> URLSession.DataTaskPublisher in
+            var redRequest = URLRequest(url: (lightsEndpoint!.appendingPathComponent(light.id).appendingPathComponent("state")))
+            redRequest.httpMethod = "PUT"
+            let state = LightState(bri: 100, hue: 56100)
+            redRequest.httpBody = try! JSONEncoder().encode(state)
+//            print(redRequest)
+            return session.dataTaskPublisher(for: redRequest)
         })
-    }
-
+        return lightChangePublishers[0].zip(lightChangePublishers[1], lightChangePublishers[2])
+}
+.sink(receiveCompletion: { (completion) in
+    
+}) { (value) in
+    print(try! JSONSerialization.jsonObject(with: value.0.data, options: []))
+    print(try! JSONSerialization.jsonObject(with: value.1.data, options: []))
+    print(try! JSONSerialization.jsonObject(with: value.2.data, options: []))
+}
+//    .sink(receiveCompletion: { (completion) in
+//        switch completion {
+//        case .failure(let error):
+//            print(error)
+//        case .finished:
+//            print("Success")
+//        }
+//    }) { (value) in
+//        try! JSONSerialization.jsonObject(with: value.0.data, options: [])
+//        try! JSONSerialization.jsonObject(with: value.1.data, options: [])
+//}
--- aloka macOS/GameViewController.swift
+++ aloka macOS/GameViewController.swift
@@ -7,6 +7,7 @@
 
 import AppKit
 import RealityKit
+import SceneKit
 
 class GameViewController: NSViewController {
     
@@ -14,9 +15,20 @@ class GameViewController: NSViewController {
     
     override func awakeFromNib() {
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let deskAnchor = try! Experience.loadDesk()
+        
+        deskAnchor.actions.touch.onAction = { (entity: Entity?) in
+            guard let entity = entity else { return }
+            guard let left = entity.findEntity(named: "barLeft") else { return }
+            guard let right = entity.findEntity(named: "barRight") else { return }
+            print(left.name)
+            print(right.name)
+//            left.components.set(ModelComponent(mesh: (left.components[ModelComponent] as! ModelComponent).mesh, materials: [SimpleMaterial(color: .systemRed, isMetallic: false)]))
+//            right.components.set(ModelComponent(mesh: (left.components[ModelComponent] as! ModelComponent).mesh, materials: [SimpleMaterial(color: .systemRed, isMetallic: false)]))
+            
+        }
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//        arView.scene.anchors.append(deskAnchor)
     }
 }
